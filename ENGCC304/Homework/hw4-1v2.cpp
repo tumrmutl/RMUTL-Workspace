413a9a8999fe48420e10f00c8f31b5fe74679737
tumrmutl <mr.kittinan@gmail.com>
2024-07-19 09:44:09 +0700

    update new file

--- ENGCC304/Homework/hw4-1v2.cpp
+++ ENGCC304/Homework/hw4-1v2.cpp
@@ -1,12 +1,26 @@
 /*
-    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+    แสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
 
     Output:
         10 20.0 Haha a
+
+
+printf( "_ _ _ _", _, _, _, _) ; 
 */
 
-int a = 10 ;
-float b = 20 ;
-char c[] = "Haha" ;
-char d = 'a' ;
-printf( "_ _ _ _", _, _, _, _) ; 
+#include <stdio.h>
+
+int main() {
+    
+    int a = 10 ;
+    float b = 20 ;
+    char c[] = "Haha" ; //String
+    char d = 'a' ;
+    printf( "\n\n" ) ;
+
+    printf( "%d %.1f %s %c", a, b, c, d ) ;
+
+    printf( "\n\n" ) ;
+
+    return 0 ;
+}//end function
